f1aa5a5ff442eb06232ad2c51410e8c670a802ac
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-15 08:05:47 +0900

    refector : input react-hook-form 모듈화

--- src/element/Input.jsx
+++ src/element/Input.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styled from "styled-components";
 
-const Input = ({}) => {
-  return <div>Input</div>;
+const Input = ({ register, placeholder, type, name, error }) => {
+  return (
+    <InputWrapper>
+      <InputCustom
+        placeholder={placeholder}
+        type={type}
+        {...register(name, {
+          required: "값을 입력해주세요.",
+        })}
+      />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </InputWrapper>
+  );
+};
+
+const InputWrapper = styled.section`
+  position: relative;
+  display: flex;
+`;
+
+const InputCustom = styled.input``;
+
+const ErrorMessage = styled.p`
+  font-size: 1.2rem;
+  color: red;
+`;
+
+Input.defaultProps = {
+  type: "text",
+  placeholder: "값을 입력하세요.",
+  register: () => {},
 };
 
 export default Input;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Button from "../element/Button";
 import Input from "../element/Input";
 const Main = () => {
+  const { register, watch, handleSubmit, formState: errors } = useForm();
+  console.log(watch());
   return (
     <div>
-      <Input />
+      <Input
+        register={register}
+        name={"username"}
+        placeholder={"이메일을 입력하세요."}
+        error={errors?.username?.message}
+        type="text"
+      />
       <Button>제출</Button>
     </div>
   );
