c971bcc6835ec79908165624da630e2f5f6cbd97
Nidurolak <jaewoon08@gmail.com>
2023-03-25 11:14:49 +0900

    Feat : 프로필 페이지 생성, 브렌치 분리 준비

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,19 @@
-import React from "react";
+import { useMutation, useQuery } from "react-query";
+import styled from "styled-components";
+import PartyRegist from "../components/modal/PartyRegist";
+import Button from "../element/Button";
+import {deletePage,deletePageMembers,getBoardDetailPage,getDetailPage,getDetailPageForAdmin,getPartyBoard,getPartyPage,PutMemberRole,undoDeletePagemembers,
+} from "../utils/api/api";
+import { partyRegistModalState, partyInfoState } from "../store/atom";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import Test from "../assets/d65d5952-d801-4225-ab16-8720733b499a.png";
+import Pagination from "react-js-pagination";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { FullDateCheck,DayCheck } from "../element/DateCheck";
+import { useSetRecoilState } from "recoil";
+import PartyInfo from "../components/party/PartyInfo";
 
 const Profile = () => {
   return <div>Profile</div>;
