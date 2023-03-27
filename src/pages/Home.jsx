9ffb2dff08dea5a08ca4c625a630974dd7fc05f1
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-27 23:15:15 +0900

    fix : home 쿠키검증

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { useRecoilValue } from "recoil";
 import Login from "../components/modal/Login";
 import { loginModalState } from "../store/atom";
+import { getCookie } from "../utils/infos/cookie";
 
 const Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = getCookie("token");
+    if (token) {
+      navigate("/main");
+    }
+  }, []);
   const loginState = useRecoilValue(loginModalState);
   return <div>{loginState === true && <Login />}</div>;
 };
