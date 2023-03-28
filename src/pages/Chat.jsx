83a7ce0aa7e9ffd24b1ec68ac94616e676e51f58
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-28 22:04:49 +0900

    test : chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,61 +1,10 @@
-import React, { useState, useEffect } from "react";
-import SockJS from "sockjs-client";
-import Stomp from "stompjs";
+import React from "react";
+import Chatting from "../components/chat/Chatting";
 
 const Chat = () => {
-  const [stompClient, setStompClient] = useState(null);
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    const socket = new SockJS("http://sparta-kdh.kro.kr/ws/chat");
-    const stompClient = Stomp.over(socket);
-    setStompClient(stompClient);
-
-    return () => {
-      stompClient.disconnect();
-    };
-  }, []);
-
-  const handleConnect = () => {
-    stompClient.connect({}, (frame) => {
-      console.log("Connected: " + frame);
-      stompClient.subscribe("/user/queue/chat", (message) => {
-        setMessages((messages) => [...messages, JSON.parse(message.body)]);
-      });
-    });
-  };
-
-  const handleSend = () => {
-    stompClient.send("/app/chat", {}, JSON.stringify({ content: message }));
-    setMessage("");
-  };
-
   return (
     <div>
-      <h1>1:1 Chat</h1>
-      {!stompClient && <button onClick={handleConnect}>Connect</button>}
-      {stompClient && (
-        <>
-          <div>
-            {messages.map((message, index) => (
-              <div key={index}>
-                <p>
-                  {message.username}: {message.content}
-                </p>
-              </div>
-            ))}
-          </div>
-          <div>
-            <input
-              type="text"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <button onClick={handleSend}>Send</button>
-          </div>
-        </>
-      )}
+      <Chatting />
     </div>
   );
 };
