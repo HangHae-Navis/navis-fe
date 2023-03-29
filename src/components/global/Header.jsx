986105b033f0751ad4c864ff85ec56843191a787
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-29 14:39:08 +0900

    feat : header menu modal

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -11,16 +11,16 @@ import { removeLocalStorage } from "../../utils/infos/localStorage";
 import Button from "../../element/Button";
 import Logo from "../../assets/logo.svg";
 import alarm from "../../assets/ic24/notification.svg";
-import chat from "../../assets/ic24/chat.svg";
 import profile from "../../assets/ic54/profile.svg";
+import { toast } from "react-toastify";
 
 const Header = () => {
   const setLoginModal = useSetRecoilState(loginModalState);
+  const [headerModal, setHeaderModal] = useState(false);
   const navi = useNavigate();
   const code = window.location.search;
   const [currentPam, setCurrentPam] = useState(code);
   const [isCallBool, setIsCallBool] = useState(false);
-  const nickname = localStorage.getItem("userInfo");
   const token = getCookie("token");
   const getCode = useQuery(
     ["getCode", currentPam],
@@ -46,6 +46,8 @@ const Header = () => {
     removeCookie("token");
     removeLocalStorage("userInfo");
     navi("/");
+    toast.success("정상적으로 로그아웃 되었습니다.");
+    onModal();
   };
 
   const onShift = () => {
@@ -56,6 +58,16 @@ const Header = () => {
       navi("/");
     }
   };
+
+  const onShiftProfile = () => {
+    navi("/main/profile");
+    onModal();
+  };
+
+  const onModal = () => {
+    setHeaderModal(!headerModal);
+  };
+
   return (
     <HeaderWrapper>
       <img src={Logo} className="logo" alt="logo" onClick={onShift} />
@@ -64,14 +76,45 @@ const Header = () => {
           Login
         </Button>
       ) : (
-        <div className="icons">
+        <div className="icons" onClick={onModal}>
           <img src={alarm} alt="알림" />
           <img src={profile} alt="프로필" />
+          {headerModal === true && (
+            <HeaderMenu>
+              <li onClick={onShiftProfile}>프로필 수정</li>
+              <li onClick={onLogout}>로그아웃</li>
+            </HeaderMenu>
+          )}
         </div>
       )}
     </HeaderWrapper>
   );
 };
+
+const HeaderMenu = styled.ul`
+  position: absolute;
+  top: 2.5rem;
+  left: 6rem;
+  width: 10rem;
+  height: 8rem;
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.color.zeroTwo};
+  border-radius: 1.6rem;
+
+  li {
+    cursor: pointer;
+    &:first-child {
+      border-bottom: 0.05rem solid #c0c0c0;
+    }
+    height: 50%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.25rem;
+  }
+`;
 
 const HeaderWrapper = styled.header`
   position: fixed;
@@ -86,6 +129,7 @@ const HeaderWrapper = styled.header`
   padding: 1rem;
   background: ${(props) => props.theme.color.zeroOne};
   .icons {
+    position: relative;
     display: flex;
     gap: 1.8rem;
     align-items: center;
