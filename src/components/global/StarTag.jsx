9de80f973a558ed7e6e0079e1d760c861c0556f2
tjdrkr2580 <tjdrkr2580@naver.com>
2023-04-14 01:03:29 +0900

    feat : survey

--- src/components/global/StarTag.jsx
+++ src/components/global/StarTag.jsx
@@ -6,7 +6,7 @@ const StarTag = ({ important }) => {
   const arr = important > 0 ? new Array(parseInt(important)).fill(0) : [];
   return (
     <>
-      {important !== undefined && important !== 0 && (
+      {important !== 0 && (
         <TagWrapper>
           {arr.map((_, i) => (
             <img src={star} alt="과제" key={i} />
